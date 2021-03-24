a8275755e696c6ce15055a665f6de10a07a2d4a9
agusNiko <73887894+agusNiko@users.noreply.github.com>
2021-03-24 17:33:45 +0100

    fixing endpoint code

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { response } = require('express');
 const express = require('express'),
 //const { unset } = require('lodash'),
   bodyParser = require('body-parser'),
@@ -8,13 +9,13 @@ const app = express();
 
 app.use(bodyParser.json());
 
-let users =[
-  {username: ' ',
-
-  email: '',
-  id: 1 
+let users = [
+  {name: 'Pedro',
+   email: ' ',
+   movies: [],
+   id: 1
   }
-]
+  ]
 
 let topMovies = [
   {
@@ -31,16 +32,16 @@ let topMovies = [
     img: ' '
   },
   {
-    title: 'TheGodfather',
+    title: 'The God father',
     director: {
-      name: ' ',
+      name: 'papa',
       bio: ' ',
       birth_year: 000,
       death_year: 000
     },
     year: 1972,
     description: '',
-    genre: ' ',
+    genre: 'Drama',
     img: ' '
     },
   {
@@ -80,7 +81,7 @@ let topMovies = [
     {
       title: 'Schindler\'s List',
       director: {
-        name: 'StevenSpielberg',
+        name: 'Spielberg',
         bio: 'is an American film director, producer, and screenwriter. He began his career in the New Hollywood era, and is one of the most commercially successful directors in history. Spielberg is the recipient of various accolades, including two Academy Awards for Best Director, a Kennedy Center honor, and a Cecil B. DeMille Award. ',
         birth_year: 1946,
         death_year: 000
@@ -139,9 +140,22 @@ let topMovies = [
       },
       year: 1999,
       description: '',
-      genre: ' ',
+      genre: 'Drama',
       img: ' '
-      }
+      },
+      {
+        title: 'Pelicula',
+        director: {
+          name: ' ',
+          bio: ' ',
+          birth_year: 000,
+          death_year: 000
+        },
+        year: 1999,
+        description: '',
+        genre: 'Drama',
+        img: ' '
+        }
 ];
 
 app.use(morgan('common'));
@@ -161,25 +175,46 @@ app.get('/movies', (req, res) => {
 //Gets the data about a single movie, by title
 
 app.get('/movies/:title' , (req,res) => {
-  res.json(topMovies.find((movie) => 
-  { return movie.title === req.params.title }));
+  res.json(topMovies.find((movie) => { return movie.title === req.params.title }));
 });
 
 //Gets the data about a single movie, by genre
 
 app.get('/movies/:title/genre' , (req,res) => {
-  res.json(topMovies.find((movie) => 
-  { movie.title === req.params.title}));
-}); //doest work
+  let thisMovie = topMovies.find((movie) => { return movie.title === req.params.title})
+  
+  let movieGenre = {'name' : thisMovie.title, genre: thisMovie.genre}
+  res.json(movieGenre);
+});
 
 //Gets the data about a director
 
-app.get('/movies/:directors' , (req,res) => {
-  res.json(topMovies.find((movies) => {
-    movies.director.name === req.params.director.directors
-  }));
-}); //Doesn't work
+app.get('/:directors' , (req,res) => {
+  let thisMovie = topMovies.find((movie) => { return movie.directors === req.params.directors})
+  let thisDirector = {'name' : thisMovie.director.name}
+  console.log(thisMovie)
+    res.json(thisDirector);
+}); //-----------------------------------Doesn't work
 
+// Gets the users list
+
+app.get('/users', (req, res) => {
+  res.json(users);
+});
+
+//add a new movie to topMovies
+app.post('/movies', (req, res) => {
+  let newMovie = req.body;
+
+  if (!newMovie.title) {
+    const message = 'Missing title in request body';
+    res.status(400).send(message);
+  } else {
+    newMovie.id = uuid.v4();
+    topMovies.push(newMovie);
+    res.status(201).send(newMovie);
+  }
+});
 
 
 //register new user
@@ -196,18 +231,45 @@ if (!newUser.name){
 });
 
 // Update the "user name" of a user
-app.put('/users/:username', (req, res) => {
-  let user = users.find((user) => { return user.username === req.params.username });
 
+app.put('/users/:username/:newUsername', (req, res) => {
+  let user = users.find((user) => { return user.name === req.params.username });
 
+  if (user) {
+    user.name = (req.params.newUsername);
+    res.status(201).json(users).send('Username '+ req.params.username + 'was change to ' + req.params.newUsername);
+  } else {
+    res.status(404).send('Student with the name ' + req.params.username + ' was not found.');
+  }
 });
 
 //add a movie to a list of favorites 
 
+app.put('/users/movies/:username/:addMovie', (req, res) =>{
+  let user = users.find((user) => { return user.name === req.params.username });
+  let newMovie = topMovies.find((movie) => { return movie.title === req.params.addMovie} );
+
+  if (user){
+      user.movies.push(newMovie)
+      res.status(201).json(user).send( req.params.addMovie.title + ' was add to favorites.');
+  }else {
+    res.status(404).send(req.params.username + ' or' + req.params.addMovie + ' was not found.');
+  }
+});
+
 //remove a movie from the list of favorites
 
-//deregister User
 
+app.delete('/users/:name', (req, res) => {
+  let user = users.find((student) => { return student.name === req.params.name });
+  console.log('pedro: ' + users)
+  
+  if (user) {
+    users = users.filter((obj) => { return obj.name !== req.params.name });
+    res.status(201).send('User ' + req.params.name + ' was deleted.');
+  }
+  console.log('pedro: ' + users)
+});
 
 app.use(express.static('public'));
 

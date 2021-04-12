5898b7e02b2ce56453d3f5d8523084324f478f23
agusNiko <73887894+agusNiko@users.noreply.github.com>
2021-04-12 20:48:25 +0200

    add Environment Variable
    change the password to a Environment Variable  keep it hide in the public repository in github

--- index.js
+++ index.js
@@ -28,7 +28,8 @@ let auth = require('./auth')(app);
 
 app.use(morgan('common'));
 
-mongoose.connect('mongodb://localhost:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true });
+//mongoose.connect('mongodb://localhost:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const cors = require('cors');
 

759474c0a36041f955f822972f3e9b522d260238
agusNiko <73887894+agusNiko@users.noreply.github.com>
2021-04-25 18:29:35 +0200

    allowing localhost1234 to cors

--- index.js
+++ index.js
@@ -39,7 +39,11 @@ mongoose.connect(process.env.CONNECTION_URI, {
 
 const cors = require("cors");
 
-let allowedOrigins = ["http://localhost:8080", "http://testsite.com"];
+let allowedOrigins = [
+  "http://localhost:8080",
+  "http://testsite.com",
+  "http://localhost:1234",
+];
 
 app.use(
   cors({

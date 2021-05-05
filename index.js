45a52e2ece0a8cf4a04da715d66c326e11c2c024
agusNiko <73887894+agusNiko@users.noreply.github.com>
2021-05-05 18:33:22 +0200

    Update index.js

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ let allowedOrigins = [
   "http://localhost:8080",
   "http://testsite.com",
   "http://localhost:1234",
-  "https://agusniko.github.io/myFlix-client",
+  "https://agusniko.github.io",
 ];
 
 app.use(
